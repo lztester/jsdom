889396ae44b0e74364814c474c5b6779e03d7e5d
lztester <108978213+lztester@users.noreply.github.com>
2022-07-09 21:58:29 +0800

    node.js version

--- test/api/from-url-in-browser.js
+++ test/api/from-url-in-browser.js
@@ -3,8 +3,8 @@ const http = require("http");
 const { assert } = require("chai");
 const { describe, it } = require("mocha-sugar-free");
 
-describe("http.request.length", () => {
-  it("http.request.length === 2", () => {
-    assert.strictEqual(http.request.length, 2);
+describe("http.request.length in node.js", () => {
+  it("http.request.length === 3", () => {
+    assert.strictEqual(http.request.length, 3);
   });
 });
