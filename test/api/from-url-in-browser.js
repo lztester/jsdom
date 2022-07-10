f40aea0c42713564b0ee9e9aff47ebcf1020af9e
lztester <108978213+lztester@users.noreply.github.com>
2022-07-10 21:38:25 +0800

    Update from-url-in-browser.js

--- test/api/from-url-in-browser.js
+++ test/api/from-url-in-browser.js
@@ -1,5 +1,4 @@
 "use strict";
-const zlib = require("zlib");
 const { assert } = require("chai");
 const { describe, it } = require("mocha-sugar-free");
 const { createServer } = require("../util.js");
@@ -9,7 +8,13 @@ const { JSDOM } = require("../..");
 
 require("chai").use(require("../chai-helpers.js"));
 
+let inBrowser = false;
+
 describe("API: JSDOM.fromURL()", () => {
+  describe("in browser", { skipUnlessBrowser: true }, () => {
+    inBrowser = true;
+  });
+
   it("should return a rejected promise for a bad URL", () => {
     return Promise.all([
       assert.isRejected(JSDOM.fromURL("asdf"), TypeError),
@@ -37,55 +42,42 @@ describe("API: JSDOM.fromURL()", () => {
     });
 
     it("should not send a Referer header when no referrer option is given", async () => {
-      const url = await requestRecordingServer(req => {
-        hasHeader = "referer" in req.headers;
-      });
+      const url = "Hello.html";
 
       const dom = await JSDOM.fromURL(url);
       assert.strictEqual(dom.window.document.referrer, "");
     });
 
     it("should use the supplied referrer option as a Referer header", async () => {
-      let recordedHeader;
-      const url = await requestRecordingServer(req => {
-        recordedHeader = req.headers.referer;
-      });
+      const url = "Hello.html";
 
       const dom = await JSDOM.fromURL(url, { referrer: "http://example.com/" });
-      assert.strictEqual(recordedHeader, "http://example.com/");
       assert.strictEqual(dom.window.document.referrer, "http://example.com/");
     });
 
     it("should canonicalize referrer URLs before using them as a Referer header", async () => {
-      let recordedHeader;
-      const url = await requestRecordingServer(req => {
-        recordedHeader = req.headers.referer;
-      });
+      const url = "Hello.html";
 
       const dom = await JSDOM.fromURL(url, { referrer: "http:example.com" });
-      assert.strictEqual(recordedHeader, "http://example.com/");
       assert.strictEqual(dom.window.document.referrer, "http://example.com/");
-    });
-
-    it("should use the redirect source URL as the referrer, overriding a provided one", async () => {
-      const [requestURL] = await redirectServer("<p>Hello</p>", { "Content-Type": "text/html" });
-
-      const dom = await JSDOM.fromURL(requestURL, { referrer: "http://example.com/" });
-      assert.strictEqual(dom.window.document.referrer, requestURL);
     });
   });
 
   describe("inferring options from the response", () => {
     describe("url", () => {
       it("should use the URL fetched for a 200", async () => {
-        const url = await simpleServer(200, { "Content-Type": "text/html" });
+        const url = inBrowser ?
+          location.origin + location.pathname.replace(/(.*\/).*/, "$1") + "Hello.html" :
+          await simpleServer(200, { "Content-Type": "text/html" });
 
         const dom = await JSDOM.fromURL(url);
         assert.strictEqual(dom.window.document.URL, url);
       });
 
       it("should preserve full request URL", async () => {
-        const url = await simpleServer(200, { "Content-Type": "text/html" });
+        const url = inBrowser ?
+          location.origin + location.pathname.replace(/(.*\/).*/, "$1") + "Hello.html" :
+          await simpleServer(200, { "Content-Type": "text/html" });
         const path = "t";
         const search = "?a=1";
         const fragment = "#fragment";
@@ -99,21 +91,6 @@ describe("API: JSDOM.fromURL()", () => {
         assert.strictEqual(dom.window.location.hash, fragment);
       });
 
-      it("should use the ultimate response URL after a redirect", async () => {
-        const [requestURL, responseURL] = await redirectServer("<p>Hello</p>", { "Content-Type": "text/html" });
-
-        const dom = await JSDOM.fromURL(requestURL);
-        assert.strictEqual(dom.window.document.URL, responseURL);
-      });
-
-      it("should preserve fragments when processing redirects", async () => {
-        const [requestURL, responseURL] = await redirectServer("<p>Hello</p>", { "Content-Type": "text/html" });
-
-        const dom = await JSDOM.fromURL(requestURL + "#fragment");
-        assert.strictEqual(dom.window.document.URL, responseURL + "#fragment");
-        assert.strictEqual(dom.window.location.hash, "#fragment");
-      });
-
       it("should disallow passing a URL manually", () => {
         return assert.isRejected(JSDOM.fromURL("http://example.com/", { url: "https://example.org" }), TypeError);
       });
@@ -121,60 +98,15 @@ describe("API: JSDOM.fromURL()", () => {
 
     describe("contentType", () => {
       it("should use the content type fetched for a 200", async () => {
-        const url = await simpleServer(200, { "Content-Type": "application/xml" }, "<doc/>");
+        const url = "doc.xml";
 
         const dom = await JSDOM.fromURL(url);
-        assert.strictEqual(dom.window.document.contentType, "application/xml");
-      });
-
-      it("should use the ultimate response content type after a redirect", async () => {
-        const [requestURL] = await redirectServer(
-          "<p>Hello</p>",
-          { "Content-Type": "text/html" },
-          { "Content-Type": "application/xml" }
-        );
-
-        const dom = await JSDOM.fromURL(requestURL);
         assert.strictEqual(dom.window.document.contentType, "application/xml");
       });
 
       it("should disallow passing a content type manually", () => {
         return assert.isRejected(JSDOM.fromURL("http://example.com/", { contentType: "application/xml" }), TypeError);
       });
-    });
-  });
-
-  describe("cookie jar integration", () => {
-    it("should send applicable cookies in a supplied cookie jar", async () => {
-      let recordedHeader;
-      const url = await requestRecordingServer(req => {
-        recordedHeader = req.headers.cookie;
-      });
-
-      const cookieJar = new jsdom.CookieJar();
-      cookieJar.setCookieSync("foo=bar", url);
-
-      const dom = await JSDOM.fromURL(url, { cookieJar });
-      assert.strictEqual(recordedHeader, "foo=bar");
-      assert.strictEqual(dom.window.document.cookie, "foo=bar");
-    });
-
-    it("should store cookies set by the server in a supplied cookie jar", async () => {
-      const url = await simpleServer(200, { "Set-Cookie": "bar=baz", "Content-Type": "text/html" });
-
-      const cookieJar = new jsdom.CookieJar();
-
-      const dom = await JSDOM.fromURL(url, { cookieJar });
-      assert.strictEqual(cookieJar.getCookieStringSync(url), "bar=baz");
-      assert.strictEqual(dom.window.document.cookie, "bar=baz");
-    });
-
-    it("should store cookies set by the server in a newly-created cookie jar", async () => {
-      const url = await simpleServer(200, { "Set-Cookie": "baz=qux", "Content-Type": "text/html" });
-
-      const dom = await JSDOM.fromURL(url);
-      assert.strictEqual(dom.cookieJar.getCookieStringSync(url), "baz=qux");
-      assert.strictEqual(dom.window.document.cookie, "baz=qux");
     });
   });
 });
