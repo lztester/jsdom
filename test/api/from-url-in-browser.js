917ab2d74f1f760ec0a21b346f04563a3ff9bf37
lztester <108978213+lztester@users.noreply.github.com>
2022-07-11 06:38:47 +0800

    Update from-url-in-browser.js

--- test/api/from-url-in-browser.js
+++ test/api/from-url-in-browser.js
@@ -1,3 +1,4 @@
+/* globals location:false */
 "use strict";
 const { assert } = require("chai");
 const { describe, it } = require("mocha-sugar-free");
@@ -19,10 +20,51 @@ describe("API: JSDOM.fromURL()", { skipUnlessBrowser: true }, () => {
     it("should reject when passing an invalid absolute URL for referrer", () => {
       assert.isRejected(JSDOM.fromURL("http://example.com/", { referrer: "asdf" }), TypeError);
     });
+
+    it("should not send a Referer header when no referrer option is given", async () => {
+      const url = location.toString();
+
+      const dom = await JSDOM.fromURL(url);
+      assert.strictEqual(dom.window.document.referrer, "");
+    });
+
+    it("should use the supplied referrer option as a Referer header", async () => {
+      const url = location.toString();
+
+      const dom = await JSDOM.fromURL(url, { referrer: "http://example.com/" });
+      assert.strictEqual(dom.window.document.referrer, "http://example.com/");
+    });
+
+    it("should canonicalize referrer URLs before using them as a Referer header", async () => {
+      const url = location.toString();
+
+      const dom = await JSDOM.fromURL(url, { referrer: "http:example.com" });
+      assert.strictEqual(dom.window.document.referrer, "http://example.com/");
+    });
   });
 
   describe("inferring options from the response", () => {
     describe("url", () => {
+      it("should use the URL fetched for a 200", async () => {
+        const url = location.toString();
+
+        const dom = await JSDOM.fromURL(url);
+        assert.strictEqual(dom.window.document.URL, url);
+      });
+
+      it("should preserve full request URL", async () => {
+        const url = location.toString();
+        const search = "?a=1";
+        const fragment = "#fragment";
+        const fullURL = url + search + fragment;
+
+        const dom = await JSDOM.fromURL(fullURL);
+        assert.strictEqual(dom.window.document.URL, fullURL);
+        assert.strictEqual(dom.window.location.href, fullURL);
+        assert.strictEqual(dom.window.location.search, search);
+        assert.strictEqual(dom.window.location.hash, fragment);
+      });
+
       it("should disallow passing a URL manually", () => {
         return assert.isRejected(JSDOM.fromURL("http://example.com/", { url: "https://example.org" }), TypeError);
       });
