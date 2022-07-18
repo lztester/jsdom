12e6127f3fafee06f7d270dfa8d790ae744dcb1f
lztester <108978213+lztester@users.noreply.github.com>
2022-07-19 07:38:26 +0800

    Update from-url-in-browser.js

--- test/api/from-url-in-browser.js
+++ test/api/from-url-in-browser.js
@@ -17,7 +17,7 @@ describe("API: JSDOM.fromURL()", { skipUnlessBrowser: true, timeout: 5000 }, () 
   });
 
   it("should return a rejected promise for a 404", () => {
-    const url = location.origin + "/base/";
+    const url = location.toString();
 
     return assert.isRejected(JSDOM.fromURL(url), Error);
   });
@@ -31,7 +31,7 @@ describe("API: JSDOM.fromURL()", { skipUnlessBrowser: true, timeout: 5000 }, () 
   describe("inferring options from the response", () => {
     describe("url", () => {
       it("should use the URL fetched for a 200", async () => {
-        const url = location.origin + "/base/";
+        const url = location.toString();
 
         const dom = await JSDOM.fromURL(url);
         assert.strictEqual(dom.window.document.URL, url);
